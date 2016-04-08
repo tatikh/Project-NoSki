01f1eda88a83ae99131fcd886e3d01a11d9e2481
tatikh <tati090578@gmail.com>
2016-04-08 15:20:13 +0300

    returns names of color`s variables changes in  .gulpfile, block 'main' for homepage

--- gulpfile.js
+++ gulpfile.js
@@ -89,6 +89,7 @@ gulp.task('watch', function() {
     watch(markupPaths.js, function() {
         gulp.start('js');
     });
+    watch(markupPaths.image, function() {
         gulp.start('images');
     });
 });
